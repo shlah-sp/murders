ae6699014d2534a5da4378735dd2e5896568041b
shlah-sp <s123ya@gmail.com>
2020-05-07 09:49:11 +0530

    First commit through github

--- download-data.R
+++ download-data.R
@@ -1,3 +1,5 @@
+#Downloading data from a flat file
+
 url <- "https://raw.githubusercontent.com/rafalab/dslabs/master/inst/extdata/murders.csv"
 dest_file <- "data/murders.csv"
 download.file(url, destfile = dest_file)
